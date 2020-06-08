df96cd466530896fc347b60e60d613f452e912dc
Vitchenko Vladislav <v.vitchenko@softaria.com>
2020-06-08 17:59:01 +0700

    schema and configuration generation

---
+++ src/main/java/model/UserChanges.java
@@ -0,0 +1,12 @@
+package model;
+
+public class UserChanges {
+    public final String userId;
+
+    public final long amountOfChanges;
+
+    public UserChanges(String userId, long amountOfChanges) {
+        this.userId = userId;
+        this.amountOfChanges = amountOfChanges;
+    }
+}
---
+++ src/main/java/model/UserMarks.java
@@ -0,0 +1,13 @@
+package model;
+
+public class UserMarks {
+
+    public final String userId;
+
+    public final long amountOfMarks;
+
+    public UserMarks(String userId, long amountOfMarks) {
+        this.userId = userId;
+        this.amountOfMarks = amountOfMarks;
+    }
+}
